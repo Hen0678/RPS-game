acbacb6ad2ac98d4fb0cd6277de227a18dd11905
Hen0678 Cohen <andycohen32@gmail.com>
2024-04-27 13:17:30 +0000

    Added choices, player, computer and results variables to JS

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// ROCK PAPER SCISSORS
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
