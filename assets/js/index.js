ae0fed3a4f24d89a324d9429c14949d775b21a42
Hen0678 Cohen <andycohen32@gmail.com>
2024-04-27 13:30:49 +0000

    Added random computer choice, it's a tie result and rock vs scissors scenario

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,20 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice) {
+// Creates a randon number between 1-3 which correlates to rock, paper or scissors     
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if(playerChoice === computerChoice) {
+        result = "IT'S A TIE";
+    }
+    else {
+        switch(playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!"
+        }
+    }
+    
+}
