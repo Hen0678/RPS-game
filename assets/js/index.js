5a097fb90bcc9f40d0fab23246d2bac37913ecd4
Hen0678 Cohen <andycohen32@gmail.com>
2024-04-27 13:37:08 +0000

    Added paper vs rock and scissors vs paper plus text displays for the results

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,19 @@ function playGame(playerChoice) {
         switch(playerChoice) {
             case "rock":
                 result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!"
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!"
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!"
+                break;              
+
         }
     }
     
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
 }
